39e849d8ed078689e3d62cf9da1716c5d57a789a
William Wendell <willewendell@gmail.com>
2017-12-21 14:13:12 +0200

    Fixed tmux mouse toggle

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-
 set -e
 
 _toggle_mouse() {
     old=$(tmux show -g | grep mouse | head -n 1 | cut -d' ' -f2)
-    new=""
 
     if [ "$old" = "on" ]; then
       new="off"
     else
       new="on"
     fi
-
-    tmux set -g mouse $new \;\
-         display "mouse: $new"
+    tmux set -g mouse $new \; display "mouse: $new"
 }
 
-
-
+"$@"
