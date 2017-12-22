94ee33be0ffe42d2823a16fe038f87c575006659
William Wendell <willewendell@gmail.com>
2017-12-22 22:35:00 +0200

    now with export mode

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ else
 fi
 
 # do options
-while getopts "dSp:?" opt; do
+while getopts "dSp:?e" opt; do
     case $opt in
         d)
             deps=1
@@ -26,6 +26,9 @@ while getopts "dSp:?" opt; do
         p)
             pkg=$OPTARG
         ;;
+        e)
+            ex_mode=1
+         ;;
         ?)
             echo << EOF
 Usage: install.sh [-opts]
@@ -33,6 +36,9 @@ Options:
     -d              Download and install dependencies
     -S              Dont sudo (when not rooted already)
     -p  pkgmgr      define package manager instead of relying crappy autodetection
+    -e              Export mode, download what we can and package it all up.
+                        Ignores dependancies, since we cant really know what
+                        your target system is like.
 EOF
 exit 0
         ;;
@@ -49,7 +55,7 @@ done
 
 # if -p is not set and we're supposed to install dependencys
 # select the most appropriate one
-[ -z ${pkg+x} ] && [[ $deps == 1 ]] && { 
+[ -z ${pkg+x} ] && [[ $deps > 0 ]] && { 
     [ -z ${pkg+x} ] && command -v apt-get >/dev/null 2>&1 && { pkg="apt-get"; };
     [ -z ${pkg+x} ] && command -v yum >/dev/null 2>&1 && { pkg="yum"; };
     [ -z ${pkg+x} ] && command -v pkg >/dev/null 2>&1 && { pkg="pkg"; };
@@ -74,49 +80,59 @@ done
 # check required commands
 command -v git >/dev/null 2>&1 || { echo >&2 "git is required, but its not installed. Try running with -d"; exit 1;  }
 
-# download submodules
-git submodule update --init --recursive
+if [ -z ${ex_mode+x} ]; then
 
-# make dirs'n'files
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-touch ~/.vimrc.local
+    # download submodules
+    git submodule update --init --recursive
 
-# Install pathogen
-cd ~/.vim/autoload
-ln -sfv $wd/vim-pathogen/autoload/pathogen.vim pathogen.vim
+    # make dirs'n'files
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    touch ~/.vimrc.local
 
-# Install vim plugins
-cd ~/.vim/bundle/
-for d in $wd/vim-plugins/*/; do (ln -svf ${d%*/} ${d##*/}); done
+    # Install pathogen
+    cd ~/.vim/autoload
+    ln -sfv $wd/vim-pathogen/autoload/pathogen.vim pathogen.vim
 
-# Link files to appropriate locations
-cd
-ln -sfv $wd/bash_aliases.bash .bash_aliases
-ln -sfv $wd/inputrc.bash .inputrc
-ln -sfv $wd/vimrc.vim .vimrc
-ln -sfv $wd/vim_help.vim .vim_help
-ln -sfv $wd/tmux.conf .tmux.conf
-ln -sfv $wd/tmux.conf.sh .tmux.conf.sh
+    # Install vim plugins
+    cd ~/.vim/bundle/
+    for d in $wd/vim-plugins/*/; do (ln -svf ${d%*/} ${d##*/}); done
+
+    # Link files to appropriate locations
+    cd
+    ln -sfv $wd/bash_aliases.bash .bash_aliases
+    ln -sfv $wd/inputrc.bash .inputrc
+    ln -sfv $wd/vimrc.vim .vimrc
+    ln -sfv $wd/vim_help.vim .vim_help
+    ln -sfv $wd/tmux.conf .tmux.conf
+    ln -sfv $wd/tmux.conf.sh .tmux.conf.sh
 
 
-# Check if source .bash_aliases already present in .bashrc and add it there if not
-check=`grep "bash_aliases" ~/.bashrc`
-if [ "$check" ]
-then
-    echo "Source ~/.bash_aliases already present in .bashrc"
-else
-    echo "Added source ~/.bash_aliases to .bashrc"
-    printf "\n# Source bash aliases\nsource ~/.bash_aliases" >> ~/.bashrc
-fi
+    # Check if source .bash_aliases already present in .bashrc and add it there if not
+    check=`grep "bash_aliases" ~/.bashrc`
+    if [ "$check" ]
+    then
+        echo "Source ~/.bash_aliases already present in .bashrc"
+    else
+        echo "Added source ~/.bash_aliases to .bashrc"
+        printf "\n# Source bash aliases\nsource ~/.bash_aliases" >> ~/.bashrc
+    fi
 
-# Add git configurations to system
-if [ -f ~/.gitconfig ]; then
-    checkContent=`grep "\[user\]" ~/.gitconfig`
-    if [ "$checkContent" ]; then
-        content=`head -3 ~/.gitconfig`
-        cp -f $wd/gitconfig.ini ~/.gitconfig
-        echo -e "$content\n$(cat ~/.gitconfig)" > ~/.gitconfig
-        echo "Updated gitconfig"
+    # Add git configurations to system
+    if [ -f ~/.gitconfig ]; then
+        checkContent=`grep "\[user\]" ~/.gitconfig`
+        if [ "$checkContent" ]; then
+            content=`head -3 ~/.gitconfig`
+            cp -f $wd/gitconfig.ini ~/.gitconfig
+            echo -e "$content\n$(cat ~/.gitconfig)" > ~/.gitconfig
+            echo "Updated gitconfig"
+        else
+            printf "Adding Git configuration\n"
+            read -p "First and last name: " name
+            read -p "E-mail address: " email
+            cp -f $wd/gitconfig.ini ~/.gitconfig
+            user="[user]\n    name = $name\n    email = $email\n"
+            echo -e "$user$(cat ~/.gitconfig)" > ~/.gitconfig
+        fi
     else
         printf "Adding Git configuration\n"
         read -p "First and last name: " name
@@ -125,13 +141,23 @@ if [ -f ~/.gitconfig ]; then
         user="[user]\n    name = $name\n    email = $email\n"
         echo -e "$user$(cat ~/.gitconfig)" > ~/.gitconfig
     fi
+
+    echo "System files updated!";
+
 else
-    printf "Adding Git configuration\n"
-    read -p "First and last name: " name
-    read -p "E-mail address: " email
-    cp -f $wd/gitconfig.ini ~/.gitconfig
-    user="[user]\n    name = $name\n    email = $email\n"
-    echo -e "$user$(cat ~/.gitconfig)" > ~/.gitconfig
+    echo ""
+    
+    if [ -f "rc.run" ]; then 
+        rm -f rc.run 
+    fi
+    if [ ! -x "makeself/makeself.sh" ]; then
+        git clone https://github.com/megastep/makeself
+    fi
+
+    echo "Exporting to rc.run..."
+    makeself/makeself.sh --notemp . rc.run "Basic bash settings and programs" echo "Extraction complete! you may now install by running install.sh"
+
+    echo "Export complete!"
+
 fi
 
-echo "System files updated!"
